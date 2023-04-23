b24dcdc2a531e3cbfa1ef959a1061bbb39e0995d
JCPedroza <nemesis4go10@gmail.com>
2023-04-22 22:28:03 -0600

    feat: implement rectangle playfield

--- 01-graphics/02-rectangle-field/rf.asm
+++ 01-graphics/02-rectangle-field/rf.asm
@@ -6,43 +6,91 @@
     seg Code
     org $F000
 
-Start:
+Reset:
     CLEAN_START
 
-NewFrame:
-    lda #2
-    ldx #0
+    lda #2              ; Constant for turning on vsync and vblank
+    ldx #0              ; Constant for turning off stuff
 
-    sta VSYNC
-    sta WSYNC
-    sta WSYNC
-    sta WSYNC
-    stx VSYNC
+    ldy #$80
+    sty COLUBK          ; Blue background color
 
+    ldy #$1C
+    sty COLUPF          ; Yellow playfield color
+
+StartFrame:
+    ; Let the TIA output 3 lines of vsync
+    sta VSYNC           ; Turn on vsync
+    REPEAT 3
+        sta WSYNC       ; Three lines for vsync
+    REPEND
+    stx VSYNC           ; Turn off vsync
+
+    ; Let the TIA output the 37 recommended lines of VBLANK
+    sta VBLANK          ; Turn on vblank
+    REPEAT 37
+        sta WSYNC       ; Thirty-seven lines for vblank
+    REPEND
+    stx VBLANK          ; Turn off vblank
+
+    ; Set playfield reflect on
+    ldy #%00000001
+    sty CTRLPF
+
+    ; Draw 192 visible lines, starting with 7 lines of no playfield
+    stx PF0
+    stx PF1
+    stx PF2
+    REPEAT 7
+        sta WSYNC       ; Seven lines with no playfield
+    REPEND
+
+    ; Draw upper wall for 7 lines
+    ldy #%11100000      ; The four less significant bits are ignored
+    sty PF0
+    ldy #%11111111
+    sty PF1
+    sty PF2
+    REPEAT 7
+        sta WSYNC       ; Seven lines of upper wall
+    REPEND
+
+    ; Draw 164 lines lines of right and left wall
+    ldy #%00100000
+    sty PF0
+    stx PF1
+    stx PF2
+    REPEAT 164
+        sta WSYNC
+    REPEND
+
+    ; Draw lower wall for 7 lines
+    ldy #%11100000      ; The four less significant bits are ignored
+    sty PF0
+    ldy #%11111111
+    sty PF1
+    sty PF2
+    REPEAT 7
+        sta WSYNC       ; Seven lines of lower wall
+    REPEND
+
+    ; Draw 7 lines of no playfield
+    stx PF0
+    stx PF1
+    stx PF2
+    REPEAT 7
+        sta WSYNC       ; Seven lines with no playfield
+    REPEND
+
+    ; Output 30 vblank overscan lines to complete the frame
     sta VBLANK
-    ldy #37
-Prescan:
-    sta WSYNC
-    dey
-    bne Prescan
+    REPEAT 30
+        sta WSYNC
+    REPEND
     stx VBLANK
 
-    ldy #192
-Draw:
-    sta WSYNC
-    dey
-    bne Draw
-
-    sta VBLANK
-    ldy #30
-Overscan:
-    sta WSYNC
-    dey
-    bne Overscan
-    stx VBLANK
-
-    jmp NewFrame
+    jmp StartFrame
 
     org $FFFC
-    .word Start
-    .word Start
+    .word Reset
+    .word Reset
